a55f6f89d42154c87916d40a4c4aed720353c9ea
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-12-16 18:29:36 +0100

    feat: profiles now support users profile pictures

--- src/users/models.py
+++ src/users/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def user_pic_location(instance, filename):
+    return f'profile_pics/{instance.user.username}/{filename}'
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about_me = models.TextField(null=True)
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to=user_pic_location)
 
     def __str__(self):
         return f'{self.user.username}\'s profile'
