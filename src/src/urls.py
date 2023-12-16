ed1ba92cd73cfb86d2ebc41f788e77c4bc2225ac
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-12-16 19:03:53 +0100

    feat: during debug, urlpatters contain urls to local static files

--- src/src/urls.py
+++ src/src/urls.py
@@ -3,11 +3,17 @@ URL configuration for src project.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('', include('homepage.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
