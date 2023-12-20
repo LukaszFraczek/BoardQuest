420739a4d7475a98eb3f1433fb3951f565486c16
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-12-20 18:04:35 +0100

    refactor: registraqtion form refactor

--- src/users/forms.py
+++ src/users/forms.py
@@ -10,20 +10,15 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserRegisterForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Custom field labels
-        self.fields['username'].label = 'Username'
-        self.fields['email'].label = 'E-mail'
-        self.fields['password1'].label = 'Password'
-        self.fields['password2'].label = 'Password Confirmation'
-
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        labels = {
+            "username": "Username",
+            "email": "E-mail",
+            "password1": "Password",
+            "password2": "Password Confirmation",
+        }
 
 
 # -------------- BAJZEL --------------
