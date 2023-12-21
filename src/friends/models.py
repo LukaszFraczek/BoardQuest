33fd0c83345857db02128a8583a2366e79a9c9ed
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-12-21 15:06:41 +0100

    feat: friends model added

--- src/friends/models.py
+++ src/friends/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils import timezone
+
+
+class Friends(models.Model):
+    """Model representing two users being friends"""
+
+    user_one = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friends")
+    user_two = models.ForeignKey(User, on_delete=models.CASCADE)
+    since = models.DateTimeField(auto_now_add=timezone.now)
+
+    class Meta:
+        unique_together = ('user', 'friend')
+
+    def __str__(self):
+        return f"Pair of friends: {self.user_one}, {self.user_two}"
+
+
+class FriendInvitation(models.Model):
+    """Model representing an invitation to ones friendlist"""
+
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invites_sent")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=timezone.now)
+
+    class Meta:
+        unique_together = ('sender', 'receiver')
+
+    def __str__(self):
+        return f"{self.sender} wants to friends with {self.receiver}"
+
