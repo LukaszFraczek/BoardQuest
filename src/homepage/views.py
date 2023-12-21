db7eaf532fa42610350f5eea5999ede3e5b84902
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2023-12-21 04:34:31 +0100

    feat: custom 404 page added

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homepage/home.html')
+
+
+def handling_404(request, exception):
+    return render(request, '404.html', {})
