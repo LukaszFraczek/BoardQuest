61414baacc383a5670b060037c8598db36c9fbd2
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-02 16:41:15 +0100

    refactor: refactored views to use common template

--- src/friends/views.py
+++ src/friends/views.py
@@ -9,12 +9,12 @@ from .models import FriendList, FriendInvitation
 
 class FriendsListView(LoginRequiredMixin, ListView):
     template_name = 'friends/friendlist.html'
-    context_object_name = 'friends'
+    context_object_name = 'users'
 
     def get_queryset(self):
         friend_list = get_object_or_404(FriendList, user=self.kwargs['pk'])
         friends = friend_list.friends.all()
-        return Profile.objects.filter(user__in=friends)
+        return User.objects.filter(id__in=friends)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,15 +24,15 @@ class FriendsListView(LoginRequiredMixin, ListView):
 
 class FriendSearchView(LoginRequiredMixin, ListView):
     template_name = 'friends/search.html'
-    context_object_name = 'search_result'
+    context_object_name = 'users'
 
     def get_queryset(self):
-        queryset = User.objects.all()
+        queryset = None
 
         # Search for specified username in querystring
         username = self.request.GET.get('username')
         if username:
-            queryset = queryset.filter(username__icontains=username)
+            queryset = User.objects.filter(username__icontains=username)
 
         return queryset
 
