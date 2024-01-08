049923b869b800ff5d73b2d85019b0d7879ae52b
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-08 18:52:47 +0100

    feat: basic model added

--- src/boardgames/models.py
+++ src/boardgames/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BoardGame(models.Model):
+    """Model representing generic boardgame"""
+
+    bgg_id = models.IntegerField(null=False, blank=False)
+    name = models.TextField(null=False, blank=False)
+    description = models.TextField(null=True)
+    release_date = models.DateTimeField(null=True)
+    players_min = models.IntegerField(null=True)
+    players_max = models.IntegerField(null=True)
+    image = models.URLField(null=True)
+    thumbnail = models.URLField(null=True)
+    average_rating = models.FloatField(null=True)
+
+    class Meta:
+        unique_together = ('id', 'bgg_id')
