5d280649162104dbbf0362756af5e386c543e976
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-10 23:49:57 +0100

    feat: thumbnail support added

--- src/boardgames/utils.py
+++ src/boardgames/utils.py
@@ -5,10 +5,10 @@ from icecream import ic
 
 
 class BGGSearch:
-    BGG_API_URL = 'https://www.boardgamegeek.com/xmlapi2/search'
+    BGG_API_URL = 'https://www.boardgamegeek.com/xmlapi2'
 
     @staticmethod
-    def __clean_xml_response(response):
+    def __clean_items_response(response):
         # Parse the XML response
         root = ElementTree.fromstring(response.content)
 
@@ -24,18 +24,53 @@ class BGGSearch:
             })
         return games
 
+    @staticmethod
+    def __clean_thumbnails_response(response):
+        # Parse the XML response
+        root = ElementTree.fromstring(response.content)
+
+        # Extract thumbnail from each game
+        thumbnails = []
+        for item in root.findall('.//item'):
+            game_id = item.get('id')
+            thumbnail = item.find('./thumbnail').text.strip()
+
+            thumbnails.append({
+                'id': game_id,
+                'thumbnail': thumbnail,
+            })
+
+        ic(thumbnails)
+
+        return thumbnails
+
     @classmethod
     def fetch_items(cls, query):
         # Parameters for the search query
         params = {'query': query, 'type': 'boardgame'}
 
         # Make the request to the API
-        response = requests.get(cls.BGG_API_URL, params=params)
+        response = requests.get(f"{cls.BGG_API_URL}/search", params=params)
 
-        ic(response.content)
+        ic("search call to BGG")
+        ic(response)
 
         if response.status_code == 200:
-            return cls.__clean_xml_response(response)
+            return cls.__clean_items_response(response)
+        else:
+            print(f"BGG request error: {response.status_code}")
+            return None
+
+    @classmethod
+    def fetch_thumbnails(cls, *args):
+        params = {'id': ','.join(*args)}
+        response = requests.get(f"{cls.BGG_API_URL}/thing/", params=params)
+
+        ic("thumbnail call to BGG")
+        ic(response)
+
+        if response.status_code == 200:
+            return cls.__clean_thumbnails_response(response)
         else:
             print(f"BGG request error: {response.status_code}")
             return None
--- src/boardgames/views.py
+++ src/boardgames/views.py
@@ -6,6 +6,8 @@ from django.views.generic import ListView, DetailView
 from .models import BoardGame
 from .utils import BGGSearch
 
+from icecream import ic
+
 
 class BoardgameSearchView(LoginRequiredMixin, ListView):
     template_name = 'boardgames/search.html'
@@ -15,7 +17,31 @@ class BoardgameSearchView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         name = self.request.GET.get('name')
         if name:
-            queryset = BGGSearch.fetch_items(name)
+            games_list = BGGSearch.fetch_items(name)
+
+            # Extract game IDs
+            # game_ids = [game.get('id') for game in games_list]
+
+            # ic(game_ids)
+
+            # Use IDs to make a second API call for thumbnails
+            # thumbnails = BGGSearch.fetch_thumbnails(game_ids)
+
+            # Combine game information with their thumbnails.
+            queryset = []
+            for game in games_list:
+                game_id = game.get('id')
+                game_name = game.get('name')
+                # thumbnail_url = [item['thumbnail'] for item in thumbnails if item['id'] == game_id],
+
+                queryset.append(
+                    {
+                        'id': game_id,
+                        'name': game_name,
+                        # 'thumbnail': thumbnail_url,
+                    }
+                )
+
             return queryset
         return []
 
