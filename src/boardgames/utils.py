a9933fe46133b32e73c33d51a71860973d13aad6
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-12 21:55:30 +0100

    feat!: Removed thumbnail fetch functionality

--- src/boardgames/utils.py
+++ src/boardgames/utils.py
@@ -24,26 +24,6 @@ class BGGSearch:
             })
         return games
 
-    @staticmethod
-    def __clean_thumbnails_response(response):
-        # Parse the XML response
-        root = ElementTree.fromstring(response.content)
-
-        # Extract thumbnail from each game
-        thumbnails = []
-        for item in root.findall('.//item'):
-            game_id = item.get('id')
-            thumbnail = item.find('./thumbnail').text.strip()
-
-            thumbnails.append({
-                'id': game_id,
-                'thumbnail': thumbnail,
-            })
-
-        ic(thumbnails)
-
-        return thumbnails
-
     @classmethod
     def fetch_items(cls, query):
         # Parameters for the search query
@@ -61,20 +41,6 @@ class BGGSearch:
             print(f"BGG request error: {response.status_code}")
             return None
 
-    @classmethod
-    def fetch_thumbnails(cls, *args):
-        params = {'id': ','.join(*args)}
-        response = requests.get(f"{cls.BGG_API_URL}/thing/", params=params)
 
-        ic("thumbnail call to BGG")
-        ic(response)
-
-        if response.status_code == 200:
-            return cls.__clean_thumbnails_response(response)
-        else:
-            print(f"BGG request error: {response.status_code}")
-            return None
-
-
-class BGGItemSearch:
+class BGGItemDetails:
     pass
