d18aea305957a5e2935fddabc22c1c79e6e2e18e
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-13 19:34:07 +0100

    feat: added cache functionality for search view

--- src/boardgames/utils.py
+++ src/boardgames/utils.py
@@ -3,6 +3,13 @@ from xml.etree import ElementTree
 
 from icecream import ic
 
+BGG_API_URL = 'https://www.boardgamegeek.com/xmlapi2'
+SEARCH_CACHE_PERSISTENCE = 300
+
+
+def get_search_key(query: str) -> str:
+    return f'bgg_search_{query}'
+
 
 class BGGSearch:
     BGG_API_URL = 'https://www.boardgamegeek.com/xmlapi2'
@@ -25,20 +32,29 @@ class BGGSearch:
         return games
 
     @classmethod
-    def fetch_items(cls, query):
+    def fetch_items(cls, name):
+        # First check if data is already in cache
+        cache_key = get_search_key(name)
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            return cached_data
+
         # Parameters for the search query
-        params = {'query': query, 'type': 'boardgame'}
+        params = {'query': name, 'type': 'boardgame'}
 
         # Make the request to the API
-        response = requests.get(f"{cls.BGG_API_URL}/search", params=params)
+        response = requests.get(f"{BGG_API_URL}/search", params=params)
 
         ic("search call to BGG")
         ic(response)
 
         if response.status_code == 200:
-            return cls.__clean_items_response(response)
+            clean_response = cls.__clean_items_response(response)
+            cache.set(cache_key, clean_response, SEARCH_CACHE_PERSISTENCE)
+            return clean_response
         else:
-            print(f"BGG request error: {response.status_code}")
+            print(f"BGG fetch items request error: {response.status_code}")
             return None
 
 
