e20dd35417719152709ed3c0a5e18600165cdd6f
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-27 19:30:16 +0100

    refactor: detail views now use single template. GameDetailViewBGG only displays games from bgg API, and GameDetailView displays games from DB

--- src/games/views/details.py
+++ src/games/views/details.py
@@ -10,7 +10,26 @@ from icecream import ic
 
 
 class GameDetailViewBGG(TemplateView):
-    template_name = 'games/details_bgg.html'
+    template_name = 'games/details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        bgg_id = self.kwargs.get('bgg_id')
+
+        if not bgg_id:
+            return HttpResponseNotFound
+
+        game_details = BGGItemDetails.fetch_item(bgg_id)
+
+        context['form'] = GameRequestForm(initial=game_details)
+        context['object'] = game_details
+        context['bgg_detail_url'] = settings.BGG_GAME_DETAIL_URL
+        return context
+
+
+class GameDetailView(DetailView):
+    template_name = 'games/details.html'
+    model = Game
 
     def get_game_request_data(self, game_obj) -> dict:
         """Returns context data for game request"""
@@ -24,59 +43,18 @@ class GameDetailViewBGG(TemplateView):
 
         if game_request.exists():
             users = game_request.first().users.all()
-            data['user_sent_request'] = True if self.request.user in users else False
-            data['request_exist'] = True
-            data['users_amount'] = users.count()
             data['request_id'] = game_request.first().id
-        else:
-            data['request_exist'] = False
+            data['user_sent_request'] = True if self.request.user in users else False
+            data['users_amount'] = users.count()
 
         return data
 
-    def get_game_values(self, bgg_id) -> (dict, dict):
-        context = dict()
-        game = Game.objects.filter(bgg_id=bgg_id)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-        if game.exists():
-            ic("Game fetched from DB")
-            game_obj = game.first()
-            game_details = game.values().first()
-
-            game_request_data = self.get_game_request_data(game_obj)
+        if self.object.status == Game.Status.REQUESTED:
+            game_request_data = self.get_game_request_data(self.object)
             context.update(game_request_data)
-        else:
-            ic("Game fetched BGG API")
-            game_details = BGGItemDetails.fetch_item(bgg_id)
 
-        return context, game_details
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        bgg_id = self.kwargs.get('bgg_id')
-
-        if not bgg_id:
-            return HttpResponseNotFound
-
-        game_context, game_details = self.get_game_values(bgg_id)
-
-        context.update(game_context)
-        context['form'] = GameRequestForm(initial=game_details)
-        context['details'] = game_details
-        context['bgg_detail_url'] = settings.BGG_GAME_DETAIL_URL
-        return context
-
-
-class GameDetailView(DetailView):
-    template_name = 'games/details.html'
-    model = Game
-
-    def get_queryset(self):
-        # display only supported games!
-        queryset = super().get_queryset()
-        queryset = queryset.filter(status=Game.Status.SUPPORTED)
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
         context['bgg_detail_url'] = settings.BGG_GAME_DETAIL_URL
         return context
