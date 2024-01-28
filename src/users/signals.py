0f3c6116ba41710128a376c03ac8beb8c588b217
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-28 17:08:11 +0100

    feat: user game list now created via signal. User data creation is created in one transaction

--- src/users/signals.py
+++ src/users/signals.py
@@ -1,13 +1,22 @@
+from django.db import IntegrityError, transaction
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+from icecream import ic
+
 from users.models import Profile
 from friends.models import FriendList
+from games.models import GameList
 
 
 @receiver(post_save, sender=User)
 def create_new_user_records(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-        FriendList.objects.create(user=instance)
+        try:
+            with transaction.atomic():
+                Profile.objects.create(user=instance)
+                FriendList.objects.create(user=instance)
+                GameList.objects.create(user=instance)
+        except IntegrityError:
+            ic("Error while creating new user")
