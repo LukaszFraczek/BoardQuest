eae53a99fbb808ff72ff8b45fc6b418da6cc208e
LukaszFraczek <lukasz.fraczek.polsl@gmail.com>
2024-01-28 17:06:13 +0100

    feat: added basic admin utiliteis for game app.

--- src/games/admin.py
+++ src/games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Game,
+    GameList,
+    GameListEntry,
+    GameRequest,
+    GameRequestUser,
+)
 
-# Register your models here.
+
+admin.site.register(Game)
+admin.site.register(GameList)
+admin.site.register(GameListEntry)
+admin.site.register(GameRequest)
+admin.site.register(GameRequestUser)
